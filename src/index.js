33534e21d0d2b13007bb06f578d88ac8a27013fd
Philip Mes <fmesy@outlook.com>
2019-03-25 22:58:16 -0300

    Code reuse and Props

--- src/index.js
+++ src/index.js
@@ -2,26 +2,39 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 //Fazer um novo App component
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 4:45pm" 
-        content="Nice blog test!"
-        avatar={faker.image.avatar()} />
-      <CommentDetail 
-        author="Alex" 
-        timeAgo="Today at 2:00Am" 
-        content="I like the subject"
-        avatar={faker.image.avatar()} />
-      <CommentDetail 
-        author="Jane" 
-        timeAgo="Yesterday at 5:00pm" 
-        content="I like the writing"
-        avatar={faker.image.avatar()} />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>        
+      </ApprovalCard>
+      <ApprovalCard>      
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 4:45pm" 
+          content="Nice blog test!"
+          avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard> 
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Today at 2:00Am" 
+          content="I like the subject"
+          avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard> 
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Yesterday at 5:00pm" 
+          content="I like the writing"
+          avatar={faker.image.avatar()} />   
+      </ApprovalCard>   
     </div>
   );
 }
